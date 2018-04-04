541584828839a69c9c406514bbc37441d90b2fae
xxj <524924091@qq.com>
2018-04-04 19:29:38 +0800

    initialize

---
+++ app/src/main/java/com/xxj/mvvm/demo/android_mvvm_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xxj.mvvm.demo.android_mvvm_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
