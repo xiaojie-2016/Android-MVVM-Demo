e8e7784d31444045a1dbb390c5f8eadd0472fd70
xxj <524924091@qq.com>
2018-04-05 12:45:49 +0800

    添加 DataBinding 依赖，修改 Activity 和 layout 文件

--- app/src/main/java/com/xxj/mvvm/demo/android_mvvm_demo/MainActivity.java
+++ app/src/main/java/com/xxj/mvvm/demo/android_mvvm_demo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.xxj.mvvm.demo.android_mvvm_demo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xxj.mvvm.demo.android_mvvm_demo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        /*
+         使用 DataBinding 布局后这里需要 DataBindingUtil 来完成 setContentView
+         返回值是布局的 ViewBinding 对象，
+         这个类是 DataBinding 框架自动生成，类文件在 app/build/intermediates/classes/yourpackage/databinding
+         修改布局后如果发现 ViewBinding 类没有及时生成，工具栏找小锤子锤（Make Project）一下就好
+        */
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
